039c413a2c8252f4de4315e9529c92a337252936
mhewedy <mhewedy@gmail.com>
2021-06-01 01:28:31 +0300

    allow swagger to pick right host

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,7 +21,6 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
 // @BasePath /api/v1
 
 // @securityDefinitions.apikey ApiKeyAuth
